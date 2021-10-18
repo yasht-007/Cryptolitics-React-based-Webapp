6bd4064425df47ea14e35674cc6435a800ee3af8
yasht-007 <yashd2d@gmail.com>
2021-10-18 11:20:58 +0530

    First commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavLinks,
+  NavItem,
+  NavBtn,
+  NavBtnLink
+} from "./NavbarElements";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">Cryptolitics</NavLogo>
+
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="/">Home</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="events">Events</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+
+            <NavItem>
+              <NavLinks to="team">Team</NavLinks>
+            </NavItem>
+
+            <NavItem>
+              <NavLinks to="contact">Contact</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+              <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
